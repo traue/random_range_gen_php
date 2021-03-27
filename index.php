af00cbbd18d8181d3248c872e323f84ac5a0a214
Thiago G. Traue <t.traue@gmail.com>
2021-03-26 21:08:18 -0300

    file name fix

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="img/dices.png">
-    <script src="js/validade.js"></script>
+    <script src="js/validate.js"></script>
     <title>Random Range Generator</title>
 </head>
 <body>
